dc25776566803274937abbee44682d2cb5737a74
danlarteygh <danlarteygh@gmail.com>
2018-02-22 01:21:43 +0000

    SPINNER

--- app/src/main/java/com/project/danlarteygh/nhis/Register.java
+++ app/src/main/java/com/project/danlarteygh/nhis/Register.java
@@ -1,5 +1,7 @@
 package com.project.danlarteygh.nhis;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,16 +9,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 
 public class Register extends AppCompatActivity implements OnItemSelectedListener {
+    Context context = this;
+    EditText editDate;
+    Calendar myCalendar = Calendar.getInstance();
+    String dateFormat = "dd.MM.yyyy";
+    DatePickerDialog.OnDateSetListener date;
+    SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +60,50 @@ public class Register extends AppCompatActivity implements OnItemSelectedListene
         // attaching data adapter to spinner
         spinner.setAdapter(dataAdapter);
 
+        editDate = (EditText) findViewById(R.id.editText_dob);
+
+// init - set date to current date
+        long currentdate = System.currentTimeMillis();
+        String dateString = sdf.format(currentdate);
+        editDate.setText(dateString);
+
+// set calendar date and update editDate
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDate();
+            }
+
+        };
+
+// onclick - popup datepicker
+        editDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(context, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
 
 
 
+
+
+
+    }
+    private void updateDate() {
+        editDate.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
@@ -61,4 +115,7 @@ public class Register extends AppCompatActivity implements OnItemSelectedListene
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
+
 }
